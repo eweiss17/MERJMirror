c72294ff61287d6312300da30ffc1323d58845a5
eweiss17 <eweiss1995@gmail.com>
2017-09-08 10:38:52 -0400

    Testing commit

--- app/src/main/java/com/merj/merjmirror/MainActivity.java
+++ app/src/main/java/com/merj/merjmirror/MainActivity.java
@@ -3,6 +3,8 @@ package com.merj.merjmirror;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/* Created By Eric Weiss */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
