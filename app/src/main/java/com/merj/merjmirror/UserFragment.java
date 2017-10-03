d5bf1100129dbf09b385e12d04b06d1e4b8f409b
eweiss17 <eweiss1995@gmail.com>
2017-10-03 00:30:14 -0400

    User page starting point, general approvements

--- app/src/main/java/com/merj/merjmirror/UserFragment.java
+++ app/src/main/java/com/merj/merjmirror/UserFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by Eric on 9/27/2017.
@@ -18,6 +20,15 @@ public class UserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.user_layout, container, false);
+
+        String[] myStringArray = {"Eric", "Megan", "James", "Ryan", "New User"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(),
+                android.R.layout.simple_list_item_1, myStringArray);
+
+        ListView listView = (ListView) myView.findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+
         return myView;
     }
 }
